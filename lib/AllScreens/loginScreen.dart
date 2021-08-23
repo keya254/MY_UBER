1afb170703d5e14a14bd9b5a4444ed9af891da2c
keya254 <brianosiemokeya@gmail.com>
2021-08-23 20:46:09 +0300

    first commit

---
+++ lib/AllScreens/loginScreen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 35.0,
+              ),
+              Image(
+                image: AssetImage("images/logo.png"),
+                width: 250.0,
+                height: 250.0,
+                alignment: Alignment.center,
+              ),
+              SizedBox(
+                height: 1.0,
+              ),
+              Text(
+                "Login as Rider",
+                style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 1.0,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          fontSize: 10.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(
+                      height: 1.0,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          fontSize: 10.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: 50.0,
+                        child: Center(
+                          child: Text(
+                            "Login",
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4.0,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print("clicked");
+                      },
+                      child: Text("Do not Have an Account? Register Here"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:my_uber/AllScreens/loginScreen.dart';
+//import 'package:my_uber/AllScreens/mainscreen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
