3654b7e2f1967af3fc807564e442ccd39b987f5c
keya254 <brianosiemokeya@gmail.com>
2021-08-25 19:17:51 +0300

    added google maps and enabled get live location

---
+++ lib/AllWidgets/configMaps.dart
@@ -0,0 +1,1 @@
+String mapKey = "AIzaSyBRihMbQ5MVxR2SU7iX2L0i2m3zDfN_CCM";
---
+++ lib/AllWidgets/divider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Dividerwidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      height:10.0,
+      color: Colors.grey,
+      thickness: 1.0,
+    );
+  }
+}
